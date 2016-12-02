f0ff058e8f22075ef5b11f35bbd06993a4ceb0a9
Matěj Kareš <karesmatej93@gmail.com>
2016-12-02 16:26:24 +0100

    Random Data Generator

---
+++ KMeans/src/utils/DataGenerator.java
@@ -0,0 +1,49 @@
+package utils;
+
+import kmeans.Point;
+
+import java.util.Random;
+
+/**
+ * Created by Matěj Kareš on 02.12.2016.
+ *
+ * Generates array of random points upon specifying dimension, quantity and spread.
+ * Data will be equally spread so this is not ideal for clustering, just to simplify the testing
+ */
+public class DataGenerator {
+
+    int dimension;
+
+    Point[] data;
+
+    int pointCount;
+
+    double spread;
+
+    public DataGenerator(int dimension, int pointCount, double spread) {
+        this.dimension = dimension;
+        this.pointCount = pointCount;
+        this.spread = spread;
+    }
+
+
+    public Point[] generateData(){
+        data = new Point[pointCount];
+
+        Random rng = new Random();
+
+        for(int i = 0; i < pointCount; i++){
+            Point generatedPoint = null;
+            generatedPoint = new Point(dimension);
+
+            for(int j = 0; j < dimension; j++){
+                double value = rng.nextDouble() * spread * 2 - spread;
+                generatedPoint.setSpecificCoord(j, value);
+            }
+
+            data[i] = generatedPoint;
+        }
+
+        return data;
+    }
+}
